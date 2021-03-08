01eb887445776717bf46f904ec17602bab69de45
luciaoSainz <danielsainz28@gmail.com>
2021-03-08 19:28:34 +0100

    added destructuring variables

--- src/index.js
+++ src/index.js
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom'
 
 
 
-const Hello = (props) => {
-
-  const name = props.name
-  const age = props.age
-
-  const bornYear = () => new  Date().getFullYear() - age;
+const Hello = ({name, age}) => { 
+const bornYear = () => new  Date().getFullYear() - age;
   
 
   return(
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
