3faf6ff9edba40712e9da6fc961d6002c77490b0
luciaoSainz <danielsainz28@gmail.com>
2021-03-12 11:31:50 +0100

    added component button

--- src/index.js
+++ src/index.js
@@ -8,19 +8,31 @@ const Display = (props) => {
   )
 }
 
+const Button = (props) => {
+  return(
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () =>{
  
   const [counter, setCounter] = useState(0)
 
   const increase = () => setCounter(counter + 1)
+  const decrease = () => setCounter(counter - 1)
 
   const setZero = () => setCounter(0)
 
 return(
    <div>
      <Display counter={counter} />
-     <button onClick={increase}>plus</button>
-     <button onClick={setZero}>zero</button>
+     <Button handleClick={increase} text='plus' />
+    
+     <Button handleClick={setZero} text='zero' />
+
+     <Button handleClick={decrease} text='minus' />
      
    </div>
   )
